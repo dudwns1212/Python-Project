b4ad1df48abde3ab9b59a6e21e4c4fd4903aab62
영준 <ernmqq@gmail.com>
2026-01-28 21:24:58 +0900

    24일차
    24일차 수업을 진행함
    1. snake 게임에서 최고 점수를 기록
    2. 코드를 종료 후 다시 시작해도 이전의 최고 점수가 보이도록 함
    3. 2를 수행하기 위해서 file을 읽고, 쓰는 방법 / 또한 with를 통해서 하나하나 file을 닫지 않아도 되는 내용을 배움
    4. 3에서 배운 file함수를 활용해서 mail-merge 프로젝트를 진행함 / split, replace를 활용하여 스스로 해결했고, 해설을 보면서 readlines(split과 동일한 file의 함수로 줄을 기준으로 배열을 만들어줌), replace, strip(앞 뒤의 공백을 제거해줌, \n도 포함) -> readlines는 배열을 만들어주는데 \n을 포함하여 생성됨, 따라서 strip으로 해당 \n의 공백을 제거함

--- day20/scoreboard.py
+++ day20/scoreboard.py
@@ -7,6 +7,8 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open("highscore_text.txt") as data:
+            self.highscore = int(data.read())
         self.penup()
         self.ht()
         self.color("white")
@@ -14,13 +16,17 @@ class Scoreboard(Turtle):
         self.update_score()
 
     def update_score(self):
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(f"Score: {self.score}  High Score: {self.highscore}", align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
         self.score += 1
-        self.clear()
         self.update_score()
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write(f"GAME OVER", align=ALIGNMENT, font=FONT)
+    def reset(self):
+        if self.score > self.highscore:
+            self.highscore = self.score
+            with open("highscore_text.txt", mode="w") as data:
+                data.write(str(self.highscore))
+        self.score = 0
+        self.update_score()
---
+++ day24/main.py
@@ -0,0 +1,13 @@
+with open(r"../../OneDrive\바탕 화면\new_file.txt") as file:
+    #C:\Users\ernmq\PycharmProjects\day24\main.py
+    contents = file.read()
+    print(contents)
+
+# with open("my_file.txt", mode="w") as file:
+#     file.write("New text")
+#
+# with open("my_file.txt", mode="a") as file:
+#     file.write("New text2")
+#
+# with open("new_file.txt", mode="w") as file:
+#     file.write("HI My new File")
